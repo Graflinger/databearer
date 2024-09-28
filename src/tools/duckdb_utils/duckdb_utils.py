6d31b32b2c2954e46cae4d5f884df8e4d778574f
Graflinger <stefangraf2@gmx.de>
2024-09-28 14:17:52 +0000

    added get duckdb connection to utils

--- src/tools/duckdb_utils/duckdb_utils.py
+++ src/tools/duckdb_utils/duckdb_utils.py
@@ -1,7 +1,12 @@
+import duckdb
+
+from src.config.parameter_handler import retrieve_parameter_value_by_name
+
+
 def create_duckdb_conform_name(name: str) -> str:
     """
     manipulates the name of a table to be conform with the
-    duckdb naming conventions 
+    duckdb naming conventions
 
     Args:
         name: str: the name of the table
@@ -13,3 +18,22 @@ def create_duckdb_conform_name(name: str) -> str:
     name = name.replace("-", "")
 
     return name
+
+
+def get_duckdb_connection(
+    duckdb_database: str = "duckdb_location",
+) -> duckdb.DuckDBPyConnection:
+    """
+    Returns a connection to a duckdb database
+
+    Args:
+        duckdb_database: str: name of the duckdb
+        database from config parameters
+
+    Returns:
+        duckdb.Connection: the connection to the duckdb database
+
+    """
+    duckdb_location = retrieve_parameter_value_by_name(duckdb_database)
+
+    return duckdb.connect(duckdb_location)
