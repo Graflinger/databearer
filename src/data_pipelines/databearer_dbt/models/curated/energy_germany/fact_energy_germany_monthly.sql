c5f5b302ca36be422dca5855848105ae5a75993f
Graflinger <stefangraf2@gmx.de>
2025-07-09 11:28:44 +0000

    renamed column

--- src/data_pipelines/databearer_dbt/models/curated/energy_germany/fact_energy_germany_monthly.sql
+++ src/data_pipelines/databearer_dbt/models/curated/energy_germany/fact_energy_germany_monthly.sql
@@ -1,7 +1,7 @@
 SELECT 
-    date_trunc('month', date) as monat,
+    date_trunc('month', date) as datum,
     AVG(anteil_eeg) as anteil_eeg
 FROM
     {{ ref('ren_share_daily_avg') }}
 GROUP BY
-    monat
+    datum
