6bbd432fa9380d0fd9b3bad33ebedb1cb790bd25
Graflinger <stefangraf2@gmx.de>
2025-07-09 11:22:14 +0000

    fixed monthly fact

--- src/data_pipelines/databearer_dbt/models/curated/energy_germany/fact_energy_germany_monthly.sql
+++ src/data_pipelines/databearer_dbt/models/curated/energy_germany/fact_energy_germany_monthly.sql
@@ -1,7 +1,7 @@
 SELECT 
-    date_part('year', date) as jahr,
+    SELECT strftime(date, '%m/%Y') as monat,
     AVG(anteil_eeg) as anteil_eeg
 FROM
     {{ ref('ren_share_daily_avg') }}
 GROUP BY
-    jahr
+    monat
