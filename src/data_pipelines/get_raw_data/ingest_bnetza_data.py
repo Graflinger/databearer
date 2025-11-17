8add65c0537f144b9762eb2b22d60c87aac106fc
Graflinger <stefangraf2@gmx.de>
2025-11-17 15:48:32 +0000

    preparation for "Windkraf Ausschraibungen 2025"

--- src/data_pipelines/get_raw_data/ingest_bnetza_data.py
+++ src/data_pipelines/get_raw_data/ingest_bnetza_data.py
@@ -11,7 +11,7 @@ logger.setLevel(logging.INFO)
 
 
 urls = {
-    'windkraft_ausschreibung': 'https://www.bundesnetzagentur.de/SharedDocs/Downloads/DE/Sachgebiete/Energie/Unternehmen_Institutionen/Ausschreibungen/Statistiken/Statistik_Onshore.xlsx?__blob=publicationFile&v=5',
+    'windkraft_ausschreibung': 'vhttps://www.bundesnetzagentur.de/DE/Fachthemen/ElektrizitaetundGas/Ausschreibungen/_DL/Statistiken/Statistik_Onshore.xlsx?__blob=publicationFile&v=11',
 }
 
 
