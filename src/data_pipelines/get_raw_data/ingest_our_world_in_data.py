49cfaf24b837e989f753e59d081fa422ad663c59
Graflinger <stefangraf2@gmx.de>
2025-11-22 10:57:55 +0000

    added owid setup

---
+++ src/data_pipelines/get_raw_data/ingest_our_world_in_data.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import logging
+
+from src.tools.datasources.world_bank_api_helper.world_bank_api_helper import (
+    get_world_bank_climate_change_knowledge_pandas_table,
+    read_metadata_from_yaml,
+)
+from src.tools.duckdb_utils.duckdb_utils import get_duckdb_connection
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def ingest_world_bank_climate_change_tables():
+    """
+    Ingests climate change data from the World Bank API into a DuckDB database.
+    """
+    queries = read_metadata_from_yaml()
+
+    for query in queries['queries']:
+        name = query["name"]
+        
+        "https://catalog.ourworldindata.org/[channel]/[dataset]/[version]/[dataset]/[table].parquet"
+        df = get_world_bank_climate_change_knowledge_pandas_table(query["value"])
+
+        with get_duckdb_connection() as con:
+            con.sql('USE staging')
+
+            con.sql(
+                f"CREATE OR REPLACE TABLE {name} AS SELECT * FROM df",
+            )
+
+            con.commit()
+
+            logging.info(f"Table {name} ingested into DuckDB")
+
+
+def main():
+    ingest_world_bank_climate_change_tables()
+
+
+if __name__ == '__main__':
+    main()
