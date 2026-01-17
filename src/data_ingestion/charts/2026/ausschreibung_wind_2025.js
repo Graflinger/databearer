13c244754ac8245491ea852ed041c415aa63c6c5
Stefan <stefangraf2@gmx.de>
2026-01-17 20:56:08 +0000

    first working version of article with all needed data visualisations

---
+++ src/data_ingestion/charts/2026/ausschreibung_wind_2025.js
@@ -0,0 +1,41 @@
+module.exports = [
+  {
+    type: "line",
+    dataFile: "wind_energy_awarded.csv",
+    outputFile: "zuschlagwert.js",
+    containerId: "zuschlagwert_wind",
+    title: "",
+    xAxisLabel: "Datum",
+    yAxisLabel: "Zuschlagswert in ct/kWh",
+    xKey: "datum",
+    yKey: "Durchschnittlicher Zuschlagswert in ct/kWh",
+    smooth: true,
+  },
+
+  {
+    type: "line",
+    dataFile: "wind_energy_awarded.csv",
+    outputFile: "zuschlagsmenge_in_kw.js",
+    containerId: "zuschlagsmenge_in_kw",
+    title: "",
+    xAxisLabel: "Datum",
+    yAxisLabel: "Menge in kW",
+    xKey: "datum",
+    seriesKeys: ["Zuschlagsmenge in kw", "Gebotsmenge in kw"],
+    seriesNames: ["Zuschlagsmenge in kw", "Gebotsmenge in kw"],
+    smooth: true,
+  },
+
+  {
+    type: "bar",
+    dataFile: "wind_energy_awarded_yearly.csv",
+    outputFile: "zuschlagsmenge_in_kw_yearly.js",
+    containerId: "zuschlagsmenge_in_kw_yearly",
+    title: "",
+    xAxisLabel: "Jahr",
+    yAxisLabel: "Zuschlagsmenge in kW",
+    xKey: "jahr",
+    yKey: "Zuschlagsmenge in kw",
+    smooth: true,
+  },
+];
